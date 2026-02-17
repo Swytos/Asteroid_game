69002bdf9f9004008b94642f1efe6a0826d42720
Swytos <svatoslavzivotovskij@gmail.com>
2026-02-17 20:02:56 +0200

    added fps controll system

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
-
+    clock = pygame.time.Clock()
+    dt = 0
     while running:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +19,7 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
