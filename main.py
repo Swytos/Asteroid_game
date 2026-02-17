7403214a292ed7b90731f73fd7d2a8079b71b771
Swytos <svatoslavzivotovskij@gmail.com>
2026-02-17 19:45:57 +0200

    black screen, add logged function

---
+++ main.py
@@ -0,0 +1,23 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+import pygame
+
+def main():
+    print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
