c6b8bd0e4d207f67e17100ad00c51f23d707dde1
Swytos <svatoslavzivotovskij@gmail.com>
2026-02-18 18:25:55 +0200

    add function of rotate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 import pygame
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
@@ -12,12 +13,16 @@ def main():
     running = True
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
