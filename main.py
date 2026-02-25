64cdde78bec553ea61154b0f8c0bbd9a254abbd5
Swytos <svatoslavzivotovskij@gmail.com>
2026-02-25 19:12:29 +0200

    add function asteroid kill

--- main.py
+++ main.py
@@ -44,6 +44,12 @@ def main():
                 log_event('player_hit')
                 print("Game Over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for obj in drawable:
             obj.draw(screen)
 
